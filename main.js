1ac7b2409dccae7449bfc5ae92447dc58feec008
leonelq2018 <leonelq2018@gmail.com>
2023-09-11 19:08:56 -0500

    funcion de menu mobile

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-
+const menuBurguer = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuBurguer.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
 }
 
-console.log("js funcionando");
+function toggleMobileMenu() {
+    console.log("la funcion nueva funciona")
+    mobileMenu.classList.toggle("inactive");
+}
+
